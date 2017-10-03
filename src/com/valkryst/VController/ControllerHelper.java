87d4b997618c96df017563a5bcacc9f9a87194ed
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-10-03 13:46:48 -0300

    Removes path related code.

--- src/com/valkryst/VController/ControllerHelper.java
+++ src/com/valkryst/VController/ControllerHelper.java
@@ -2,38 +2,13 @@ package com.valkryst.VController;
 
 import com.valkryst.VController.preset.ControllerPreset;
 import com.valkryst.VController.preset.LogitechRumblePad2;
-import lombok.Getter;
 import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ControllerHelper {
-    static {
-        try {
-            instance = new ControllerHelper();
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Getter private static ControllerHelper instance;
-
-    /**
-     * Constructs a new ControllerHelper
-     *
-     * @throws NoSuchFieldException
-     *        If there was an issue adding the libraries folder to the path.
-     *
-     * @throws IllegalAccessException
-     *        If there was an issue adding the libraries folder to the path.
-     */
-    private ControllerHelper() throws NoSuchFieldException, IllegalAccessException {
-        addLibrariesToPath();
-    }
-
     /**
      * Retrieves all connected controllers.
      *
@@ -49,7 +24,7 @@ public class ControllerHelper {
      *        cannot be loaded and if the fallback code
      *        also fails.
      */
-    public Controller[] getAllControllers() {
+    public static Controller[] getAllControllers() {
         return ControllerEnvironment.getDefaultEnvironment().getControllers();
     }
 
@@ -59,7 +34,7 @@ public class ControllerHelper {
      * @return
      *        The list of supported connected controllers.
      */
-    public List<Controller> getSupportedControllers() {
+    public static List<Controller> getSupportedControllers() {
         final Controller[] controllers = getAllControllers();
         final List<Controller> supportedControllers = new ArrayList<>();
 
@@ -87,7 +62,7 @@ public class ControllerHelper {
      * @throws UnsupportedOperationException
      *        If no preset exists for the controller.
      */
-    public ControllerPreset getControllerPreset(final Controller controller) {
+    public static ControllerPreset getControllerPreset(final Controller controller) {
         switch (controller.getName()) {
             case "Logitech Logitech RumblePad 2 USB": {
                 return new LogitechRumblePad2();
@@ -97,33 +72,5 @@ public class ControllerHelper {
                 throw new UnsupportedOperationException("No preset exists for the '" + controller.getName() + "' controller.");
             }
         }
-    }
-
-    /**
-     * Adds the libraries folder to the path.
-     *
-     * @throws NoSuchFieldException
-     *        If the sys_paths field cannot be found.
-     *
-     * @throws IllegalAccessException
-     *       If the sys_paths field is final or inaccessible.
-     */
-    public void addLibrariesToPath() throws NoSuchFieldException, IllegalAccessException {
-        String path = System.getProperty("java.library.path");
-
-        if (path.isEmpty() == false) {
-            path += ":";
-        }
-
-        path += System.getProperty("user.dir") + "/libraries/JInput";
-
-        System.setProperty("java.library.path", path);
-
-        // Set the sys_paths field to null.
-        // This will cause the library paths to be re-initialized when a library
-        // is next loaded.
-        final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
-        sysPathsField.setAccessible(true);
-        sysPathsField.set(null, null);
     }
 }
