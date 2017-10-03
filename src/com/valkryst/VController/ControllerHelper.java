e86bc5fa8c974b4d1b992b783d351f641ebd6174
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-10-03 13:24:27 -0300

    Adds code to force a refresh of the libraries after altering the path.

--- src/com/valkryst/VController/ControllerHelper.java
+++ src/com/valkryst/VController/ControllerHelper.java
@@ -5,6 +5,7 @@ import com.valkryst.VController.preset.LogitechRumblePad2;
 import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,18 +21,28 @@ public final class ControllerHelper {
      *
      * @throws UnsatisfiedLinkError
      *        If one of the libraries, required by JInput,
-     *        cannot be loaded.
+     *        cannot be laded.
+     *
+     * @throws IllegalStateException
+     *        If one of the libraries, required by JInput,
+     *        cannot be loaded and if the fallback code
+     *        also fails.
      */
     public static Controller[] getAllControllers() {
         try {
             return ControllerEnvironment.getDefaultEnvironment().getControllers();
         } catch (final UnsatisfiedLinkError e) {
-            // The exception totally ignores the catch block.
-            // Can't find a reason why.
-        } finally {
-            addLibrariesToPath();
-            return ControllerEnvironment.getDefaultEnvironment().getControllers();
+            try {
+                System.out.println("Yeah, catch block is running");
+                addLibrariesToPath();
+                return ControllerEnvironment.getDefaultEnvironment().getControllers();
+            } catch (IllegalAccessException | NoSuchFieldException ee) {
+                ee.printStackTrace();
+            }
         }
+
+        throw new IllegalStateException("Unable to load libraries. Fallback method " +
+                                        "has also failed.");
     }
 
     /**
@@ -80,8 +91,16 @@ public final class ControllerHelper {
         }
     }
 
-    /** Adds the libraries folder to the path. */
-    public static void addLibrariesToPath() {
+    /**
+     * Adds the libraries folder to the path.
+     *
+     * @throws NoSuchFieldException
+     *        If the sys_paths field cannot be found.
+     *
+     * @throws IllegalAccessException
+     *       If the sys_paths field is final or inaccessible.
+     */
+    public static void addLibrariesToPath() throws NoSuchFieldException, IllegalAccessException {
         String path = System.getProperty("java.library.path");
 
         if (path.isEmpty() == false) {
@@ -91,5 +110,12 @@ public final class ControllerHelper {
         path += System.getProperty("user.dir") + "/libraries/JInput";
 
         System.setProperty("java.library.path", path);
+
+        // Set the sys_paths field to null.
+        // This will cause the library paths to be re-initalized when a library
+        // is next loaded.
+        final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+        sysPathsField.setAccessible(true);
+        sysPathsField.set(null, null);
     }
 }
